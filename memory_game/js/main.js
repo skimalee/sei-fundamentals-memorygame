45d8f647681edbbbddc2a06b2e0c6e08518b2cc2
skimalee <skimalee@users.noreply.github.com>
2020-01-21 08:29:32 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,19 @@
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
 
-console.log("user picked " + cardThree);
-console.log("user picked " + cardTwo);
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+
+cardOne = cards[2];
+cardTwo = cards[3];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert('You found a Match!');
+		} else {
+			alert('Sorry, Try Again!');
+		}
+	}
+
+
